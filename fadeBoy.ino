b5cc9292838920f16ac0656ae3bb26b8c19a6736
juan <juanjofrelopez@gmail.com>
2021-11-02 14:57:12 -0300

    fixed some shit

--- fadeBoy.ino
+++ fadeBoy.ino
@@ -1,3 +1,5 @@
+#define MAX_BRIGHTNESS 255
+
 int bright[6] = {0, 40, 80, 120, 100, 60};
 int brightness = 0;
 int leds[6] = {3,5,6,9,10,11};
@@ -15,19 +17,14 @@ void setup() {
 
 void loop() {
   randNumber = floor(random(0,6));
-  //map(randNumber,0,1000,0,5);
   int i = randNumber;  
   Serial.println(i);
-  
-  //for(int i=0;i<6;i++){
-    brightness = bright[i];
-    analogWrite(leds[i],brightness);
-    brightness += fadeAmount[i];
-    if (brightness <= 0 || brightness >= 160) {
-      fadeAmount[i] = -fadeAmount[i];
-    }
-    bright[i] = brightness;
-  //}
-  
+  brightness = bright[i];
+  analogWrite(leds[i],brightness);
+  brightness += fadeAmount[i];
+  if (brightness <= 0 || brightness >= MAX_BRIGHTNESS) {
+    fadeAmount[i] = -fadeAmount[i];
+  }
+  bright[i] = brightness;
   delay(delayTime);  
 }
